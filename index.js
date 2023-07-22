4b11ffb00845e65f9e9c1a40837bcda75f8f876f
Juan Pablo <underkron@hotmail.com>
2023-07-22 00:02:46 -0300

    trabajo listo

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-//* prueba**/
+//* 1ra Entrega Backend
+
+class ProductManager {
+    constructor() {
+      this.products = [];
+    }
+  
+    addProduct(product) {
+      this.products.push(product);
+    }
+  
+    getProducts() {
+      return this.products;
+    }
+  
+    getProductById(id) {
+      const product = this.products.find(product => product.code === id);
+      if (product) {
+        return product;
+      } else {
+        console.error("Producto No Encontrado");
+        return null;
+      }
+    }
+  }
+  
+ 
+  const manager = new ProductManager();
+  
+  // Agregar productos
+  manager.addProduct({
+    Title: "Producto 1",
+    description: "Descripción del producto 1",
+    price: 19.99,
+    code: "P001",
+    stock: 50
+  });
+  
+  manager.addProduct({
+    Title: "Producto 2",
+    description: "Descripción del producto 2",
+    price: 29.99,
+    code: "P002",
+    stock: 30
+  });
+  
+  // Obtener todos los productos
+  const allProducts = manager.getProducts();
+  console.log(allProducts);
+  
+  // Obtener producto por ID
+  const productId = "P001";
+  const productById = manager.getProductById(productId);
+  console.log(productById);
+  
+  // Intentar obtener un producto con un ID no existente
+  const productoInexistenteId = "P003";
+  const ProductoInexistente = manager.getProductById(productoInexistenteId);
+  console.log(ProductoInexistente);
